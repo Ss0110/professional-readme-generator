e1861c404bb41faf23247d70737d4d150fedee5c
Ss0110 <saraysanchez01@yahoo.com>
2023-04-22 01:02:42 -0700

    worked on generateMarkdown file

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Professional README Generator Starter Code
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+## Table of Contents
+* [Description](#Description)
+* [User Story](#User-Story)
+* [Language/Technoligies Used](#Language/Technologies-Used) 
+* [Repository](#Repository-Link)
+
+## Description
+This app helps generate a professional README after the user answers a set of prompted questions.(title?, description?, table of contents?)
+
+## User Story
+```
+AS A developer
+I WANT a README generator
+SO THAT I can quickly create a professional README for a new project
+```
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+ 
+
+  if(license != "None") {
+    badge = "![License Badge](https://shields.io/badge/license-" + license + "-green)";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case MIT:
+      licenselink = "http://opensource.org/licenses/MIT";
+      break;
+      case Apache:
+        licenselink= "https://www.apache.org/licenses/";
+        break;
+        case GPLv3:
+          licenselink = "http://www.gnu.org/licenses/quick-guide-gplv3.html";
+          break;
+          case BSD-2-Clause: 
+          licenselink = "http://opensource.org/licenses/BSD-2-Clause";
+          break;
+          case AGPL: 
+          licenselink = "https://gnu.org/licenses/agpl.html";
+          break;
+          case LGPL:
+            licenselink = "https://www.gnu.org/licenses/lgpl.html";
+            break;
+            case CreativeCommons:
+              licenselink = "http://creativecommons.org/";
+              break;
+              case None:
+                licenselink = "";
+  }
+  return licenselink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
 `;
 }
 
